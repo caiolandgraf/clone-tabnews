99d398602ef58218038bcf235258efbcfbf3fd6d
Caio Campos <eu.caiolandgraf@gmail.com>
2026-02-13 19:24:07 -0300

    adicionar scripts `lint:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Theo, eu te amo. Se você me ama também, da uma risadinha.</h1>
+  return <h1>Theo, eu te amo. Se você me ama também, da uma risadinha.</h1>;
 }
 
-export default Home
+export default Home;
